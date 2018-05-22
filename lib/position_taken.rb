03b4d02e9eedca7d55df6c44fa77381bc06d62c8
jfburk <cineburk@gmail.com>
2018-05-22 23:16:00 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,8 @@ def position_taken?(board, index)
   
   if board[index]==" " || "" || nil
     false
-  elsif board[index]== "X" ||"O"
-    true
+  #elsif board[index]== "X" ||"O"
+    #true
   else
     return nil
   end
